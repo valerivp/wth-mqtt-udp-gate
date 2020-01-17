575499553a00dc7bf8a47e1371f8d4fd7789e22e
ValeriVP <valerivp@gmail.com>
2020-01-17 21:29:54 +0300

    no message

--- Debug/ESPAsyncTCP-master/tcp_axtls.c.d
+++
@@ -1,3 +0,0 @@
-r:\Temp\VMBuilds\wth433-mqtt-udp-gate\esp8266_d1_mini\Debug\ESPAsyncTCP-master\tcp_axtls.c.o: \
- C:\Users\ValeriVP\Documents\Arduino\libraries\ESPAsyncTCP-master\src\tcp_axtls.c \
- C:\Users\ValeriVP\Documents\Arduino\libraries\ESPAsyncTCP-master\src/async_config.h
--- Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-r:\Temp\VMBuilds\wth433-mqtt-udp-gate\esp8266_d1_mini\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\prdg5meb.0kl\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
